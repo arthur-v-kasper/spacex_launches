e3b46aeb8dab4bad27c942bb0e8a6fee582f4838
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-04-30 00:33:46 -0300

    showing api response at index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'lauch/upcoming', to: 'lauch#upcoming'
   get 'lauch/latest', to: 'lauch#latest'
   get 'lauch/next', to: 'lauch#next'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
